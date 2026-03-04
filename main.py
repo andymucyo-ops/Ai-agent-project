81d16098ac184bffe8042ca3314ac105d0d7cba2
andymucy-ops <andymucyo@gmail.com>
2026-03-04 12:38:23 +0100

    added parser
    to handle user's input

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 def main():
 
+    # loading all variables from .env file
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key is None:
@@ -11,11 +13,26 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
+    # generating argparse object that accepts users input
+    user_input = argparse.ArgumentParser(description="Chatbot")
+    user_input.add_argument("user_prompt", type=str, help="User's prompt")
+    args = user_input.parse_args()
+
     response = client.models.generate_content(
             model="gemini-2.5-flash",
-            contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+            contents=args.user_prompt
+            )
 
-    print("Response:", response.text)
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+
+    if response.usage_metadata is None:
+        raise RuntimeError("failed Api request")
+
+    # print("User prompt:", response )
+    print("Prompt tokens: ", prompt_tokens)
+    print("Response tokens: ", response_tokens)
+    print("Response:\n", response.text)
 
 if __name__ == "__main__":
     main()
