0dfb83b0db378974b65348939eb8cc8407200ae3
andymucy-ops <andymucyo@gmail.com>
2026-03-07 12:27:01 +0100

    refactor tests separators

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,9 +1,9 @@
 from functions.get_files_info import get_files_info
 
 get_files_info("calculator", ".")
-print("\n")
+print("=========================")
 get_files_info("calculator", "pkg")
-print("\n")
+print("=========================")
 get_files_info("calculator", "/bin")
-print("\n")
+print("=========================")
 get_files_info("calculator", "../")
