4fe0cf737d9d1834118fe1012342b5c505d0c277
andymucy-ops <andymucyo@gmail.com>
2026-03-08 15:22:02 +0100

    feature: write file function
    function that enable the agent to write to write to or overwride a
    specified file

---
+++ functions/write_file.py
@@ -0,0 +1,28 @@
+
+import os
+
+
+def write_file(working_dir: str, file_path: str, content: str) -> None:
+    try: 
+        abs_wd: str = os.path.abspath(working_dir)
+        abs_file_path: str = os.path.normpath(os.path.join(abs_wd,file_path))
+        valid_path: bool = os.path.commonpath([abs_wd,abs_file_path]) == abs_wd
+
+        if not valid_path: 
+            raise Exception(f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory')
+
+        if not os.path.isdir(abs_file_path):
+            raise Exception(f'Error: Cannot write to "{file_path}" as it is a directory')
+
+        os.makedirs(abs_file_path, exist_ok=True)
+
+        with open(abs_file_path, "w") as f:
+            new_file: str = f.write(content)
+
+            if new_file == content:
+                return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+
+
+    except Exception as err:
+        
+        print(err)
