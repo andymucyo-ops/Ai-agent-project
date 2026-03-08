b7a991861382bc868bf6c7909b9850aa44a57ce3
andymucy-ops <andymucyo@gmail.com>
2026-03-08 16:11:52 +0100

    bug fix: fixed return
    refactored output return typehint and modifed return to actually appear

--- functions/write_file.py
+++ functions/write_file.py
@@ -2,25 +2,28 @@
 import os
 
 
-def write_file(working_dir: str, file_path: str, content: str) -> None:
+def write_file(working_dir: str, file_path: str, content: str) -> str | None:
     try: 
         abs_wd: str = os.path.abspath(working_dir)
+
         abs_file_path: str = os.path.normpath(os.path.join(abs_wd,file_path))
+        
         valid_path: bool = os.path.commonpath([abs_wd,abs_file_path]) == abs_wd
 
         if not valid_path: 
             raise Exception(f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory')
 
-        if not os.path.isdir(abs_file_path):
+        if os.path.isdir(abs_file_path):
             raise Exception(f'Error: Cannot write to "{file_path}" as it is a directory')
 
-        os.makedirs(abs_file_path, exist_ok=True)
+        # Creating the missing parent directories, if none are missing no changes 
+        os.makedirs(os.path.dirname(abs_file_path), exist_ok=True)
 
         with open(abs_file_path, "w") as f:
-            new_file: str = f.write(content)
+            print(f"Writing to {file_path}...")
+            f.write(content)
 
-            if new_file == content:
-                return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
 
 
     except Exception as err:
